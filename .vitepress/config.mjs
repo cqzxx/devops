5827a26b25d7e3e88ae6ecd4c6f60ef7ee157569
shengan <shengan@popmart.com>
2023-12-19 10:18:05 +0800

    new

--- .vitepress/config.mjs
+++ .vitepress/config.mjs
@@ -243,6 +243,7 @@ export default defineConfig({
             { text: 'kubectl 常用命令', link: '/op/0154' },
             { text: 'k8s 创建 api token', link: '/op/7162' },
             { text: 'python 调用 k8s api', link: '/op/7636' },
+            { text: 'python k8s web terminal', link: '/op/3114' },
           ]
         },
         {
