b74bf5898d3f6fbb33f896c1c5efb25fafbbee4c
shengan <shengan@popmart.com>
2023-12-25 11:31:12 +0800

    add js object

--- .vitepress/config.mjs
+++ .vitepress/config.mjs
@@ -213,6 +213,7 @@ export default defineConfig({
             { text: 'js 基础语法', link: '/fe/8038' },
             { text: 'js 字符串', link: '/fe/1596' },
             { text: 'js 数组', link: '/fe/4380' },
+            { text: 'js 对象', link: '/fe/7577' },
             { text: 'js 代码片段', link: '/fe/1845' },
             { text: '使用 moment 库显示相对时间', link: '/fe/0858' },
             { text: '点击复制到剪切板', link: '/fe/8504' },
